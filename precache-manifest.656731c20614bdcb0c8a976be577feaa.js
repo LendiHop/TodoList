ed7f5238c10e1f47845908816c28ed95eadce55e
LendiHop <mishin.danil2017@gmail.com>
2021-08-09 22:41:05 +0300

    Updates

---
+++ precache-manifest.656731c20614bdcb0c8a976be577feaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce77c0dc0fb9b8a5a4b9ae27c124f19",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "c91cdc6da372c4456d57",
+    "url": "/TodoList/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "14db3828fe2546a16ff2",
+    "url": "/TodoList/static/js/2.dd01199b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.dd01199b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91cdc6da372c4456d57",
+    "url": "/TodoList/static/js/main.7f9b7199.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
